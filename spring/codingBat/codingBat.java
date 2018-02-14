65207aca029586f5d9a690ec4769f3c5bb080852
Sasha <akoroza@stuy.edu>
2018-02-14 05:20:52 -0500

    codingbat

--- spring/codingBat/codingBat.java
+++ spring/codingBat/codingBat.java
@@ -1,1 +1,18 @@
-public
+public class codingBat{
+
+
+    public static int bunnyEars(int n){
+	if (n==0){return 0;}
+	else if (n%2==1){
+	    return 2+ bunnyEars(n-1);
+	}
+	else{
+	    return 3+ bunnyEars(n-1);
+	}
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
