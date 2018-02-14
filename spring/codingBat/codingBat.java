cb5cf94f3839cb4070618aa6edf5918dfbb5835e
Sasha <akoroza@stuy.edu>
2018-02-14 05:22:41 -0500

    codingbat

--- spring/codingBat/codingBat.java
+++ spring/codingBat/codingBat.java
@@ -14,6 +14,9 @@ public class codingBat{
 
     public static void main(String[] args){
 	System.out.println(bunnyEars(2));
+	System.out.println(bunnyEars(0));
+	System.out.println(bunnyEars(1));
+	System.out.println(bunnyEars(3));
     }
 
 }
