bb12fa347260b4218579065167990b9e214dfa84
akoroza <akoroza>
2018-03-25 13:15:10 -0400

    dd node file done

--- spring/hw24-dlinkedlist/DLLNode.java
+++ spring/hw24-dlinkedlist/DLLNode.java
@@ -1,3 +1,10 @@
+/*
+Aleksandra Koroza, Angela Tom
+Duo Name: Angandra 
+APCS2 pd2
+HW #24a: On the DLL
+ */
+
 /*****************************************************
  * class DLLNode
  * Implements a doubly-linked node,
@@ -10,8 +17,15 @@ public class DLLNode
     private String _cargo;    //cargo may only be of type String
     private DLLNode _nextNode, _prevNode; //pointers to next, prev DLLNodes
 
-        // constructor -- initializes instance vars
-    public LLNode( String value, DLLNode next, DLL prev ) {
+    // constructor -- initializes instance vars
+
+    public DLLNode( String value ) {
+	_cargo = value;
+	_nextNode = null;
+	_prevNode = null;
+    }
+
+    public DLLNode( String value, DLLNode next, DLLNode prev ) {
 	_cargo = value;
 	_nextNode = next;
 	_prevNode = prev;
@@ -21,9 +35,9 @@ public class DLLNode
     //--------------v  ACCESSORS  v--------------
     public String getCargo() { return _cargo; }
 
-    public LLNode getNext() { return _nextNode; }
+    public DLLNode getNext() { return _nextNode; }
 
-    public LLNode getPrev() { return _prevNode; }
+    public DLLNode getPrev() { return _prevNode; }
     //--------------^  ACCESSORS  ^--------------
 
 
@@ -34,16 +48,40 @@ public class DLLNode
 	return foo;
     }
 
-    public LLNode setNext( LLNode newNext ) {
-	LLNode foo = getNext();
+    public DLLNode setNext( DLLNode newNext ) {
+	DLLNode foo = getNext();
 	_nextNode = newNext;
 	return foo;
     }
 
-    public LLNode setPrev( LLNode newPrev ) {
-	LLNode foo = getPrev();
+    public DLLNode setPrev( DLLNode newPrev ) {
+	DLLNode foo = getPrev();
 	_prevNode = newPrev;
 	return foo;
     }
+
+    //--------------^  MUTATORS  ^--------------
+
+     // override inherited toString
+    public static String toString(DLLNode obj)
+    {
+	//if (obj.value== null) {return "null";}
+	return obj._prevNode.getCargo()+"|"+ obj.getCargo()+ "|"+ obj._nextNode.getCargo();
+
+    }
+
+
+    //main method for testing
+    public static void main( String[] args )
+    {
+	DLLNode test1= new DLLNode("first");
+	DLLNode test2= new DLLNode("second");
+	DLLNode test3= new DLLNode("third");
+
+	test1.setNext(test2);
+	test1.setPrev(test3);
+	System.out.println(toString(test1));
+	
+    }//end main
 
 }//end class DLLNode
